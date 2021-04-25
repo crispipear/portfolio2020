b1942b147ed57fee49fe1bdebc14079c4b36754b
crispipear <lisuy97@gmail.com>
2021-04-24 22:30:05 -0700

    update site config/meta

--- site-config.js
+++ site-config.js
@@ -1,7 +1,7 @@
 export default {
-    siteTitle: "Su Li — design-focused engineer",
+    siteTitle: "syl. — Su Li | engineer, designer",
     siteUrl: "https://lisuying.com/",
-    description: "I'm a front end engineer and ux designer based in Seattle. Focused on creating solutions for problems where user experience, visual aesthetics and technology collide. Currently at Amazon.",
+    description: "I'm an engineer and designer specialized in front end development and ux design based in Seattle. Focused on creating solutions for problems where user experience, visual aesthetics and technology collide.",
     keywords: "su li, portfolio, crispipear, front end engineer, ux designer, ux engineer, front end developer, imd, interactive media design, uw bothell, uwb, university of washington",
     metaImageUrl: "https://lisuying.com/meta.jpg"
 }
